e23026a14bfc3c15fffca78e2519bc9aa77154bc
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-06-26 14:19:01 +0530

    sdfsd

--- Backend/index.js
+++ Backend/index.js
@@ -49,19 +49,19 @@ const expenseSchema = new mongoose.Schema({
        res.json(expenses)
 
     }catch(error){
-      console.error('Error getting expense,error')
-      res.status(500),json({error: "failed to load"});
+      console.error('Error getting expense', error)
+      res.status(500).json({error: "failed to load"});
     }
  });
 
  app.delete('/Expense/:userId', async(req,res)=>{
    try{
-   const {userID }=req.params;
-   const deleteExpense = await Expense .findByIdAndDelete(userID);
+   const { userId } = req.params;
+   const deleteExpense = await Expense.findByIdAndDelete(userId);
    if(!deleteExpense){
-      return res.status(404).json()({error : "not found"});
+      return res.status(404).json({error : "not found"});
    }else{
-   res.status(201).json({message:"expense delete",deleteExpense})
+   res.status(201).json({message:"expense deleted", deleteExpense})
    }
  } catch(error){
     console.error('error deleting expense',error)
